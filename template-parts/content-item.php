98e6a242cf3708f9bd80c97347b87642a09b83e0
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2022-02-08 11:48:55 +0900

    6-69

--- template-parts/content-item.php
+++ template-parts/content-item.php
@@ -1,1 +1,22 @@
 <p>商品名： <?php the_title(); ?></p>
+
+<?php $price = get_post_meta(get_the_ID(), '価格', true); ?>
+<?php $publisher = get_post_meta(get_the_ID(), '出版社', true); ?>
+<?php $writers = get_post_meta(get_the_ID(), '著者', false); ?>
+
+<dl>
+    <?php if ($price !== '') : ?>
+        <dt>価格</dt>
+        <dd><?php echo esc_html(number_format($price)); ?>円</dd>
+    <?php endif; ?>
+    <?php if ($publisher !== '') : ?>
+        <dt>出版社</dt>
+        <dd><?php echo esc_html($publisher) ?></dd>
+    <?php endif; ?>
+    <?php if ($writers) : ?>
+        <dt>著者</dt>
+        <?php foreach ($writers as $writer): ?>
+            <dd class="mb-0"><?php echo esc_html($writer) ?></dd>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</dl>
